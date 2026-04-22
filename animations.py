8565d32368cef92612bdf0f51a786cbe952554f5
Nguyễn Dũng <nguyendung2542007@gmail.com>
2026-04-22 12:03:59 +0700

    Update animations.py

--- animations.py
+++ animations.py
@@ -17,8 +17,8 @@ class AnimationManager: # Quản lý các trạng thái rung màn hình và các
                    random.randint(-self.shake_intensity, self.shake_intensity)
         return 0, 0
 
-    def get_pulse(self, current_time, speed=0.003): # Tính toán giá trị nhịp đập
+    def get_pulse(self, current_time, speed=0.003): # Tính toán giá trị nhịp đập (hiện tại chưa dùng)
         return (math.sin(current_time * speed) + 1) / 2
 
-    def get_blink(self, current_time, speed=0.005): # Tính toán giá trị nhấp nháy cho ô cờ
+    def get_blink(self, current_time, speed=0.005): # Tính toán giá trị nhấp nháy cho ô cờ (Khi summon)
         return (math.sin(current_time * speed) + 1) / 2
